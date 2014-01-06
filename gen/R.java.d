e24984e2adb8978837ff31bca57486424ce7feaf
Ben Mathes <mathes.ben@gmail.com>
2014-01-05 17:30:34 -0800

    compliations

---
+++ gen/R.java.d
@@ -0,0 +1,18 @@
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/gen/org/stuff/events/R.java \
+ : /Users/benmathes/programming/personal/clojure_android_tutorial/events/res/anim/splash_rotation.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable/splash_background.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_circle.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_droid.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_hands.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-ldpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-mdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/layout/splashscreen.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/values/strings.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_circle.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_droid.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_hands.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-ldpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-mdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/AndroidManifest.xml \
---
+++ target/events.ap_.d
@@ -0,0 +1,19 @@
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/events.ap_ \
+ : /Users/benmathes/programming/personal/clojure_android_tutorial/events/res/anim/splash_rotation.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable/splash_background.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_circle.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_droid.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-hdpi/splash_hands.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-ldpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/drawable-mdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/layout/splashscreen.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/res/values/strings.xml \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_circle.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_droid.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-hdpi/splash_hands.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-ldpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/target/res/drawable-mdpi/ic_launcher.png \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/assets/data_readers.clj \
+/Users/benmathes/programming/personal/clojure_android_tutorial/events/AndroidManifest.xml \
